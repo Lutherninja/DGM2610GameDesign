0b84b443a28ea180b8bb0d88e12e9b88d75b2720
Unknown <Luther.ninja@gmail.com>
2019-02-07 14:45:36 -0700

    Created new Prototype so I can use it at school and home

---
+++ LabbyPrototype/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Scene))]
+
+public class DeathZone : MonoBehaviour{
+ 
+
+//private void currentScene;
+
+
+
+
+  //  private void OnTriggerEnter(Collider other)
+ //   {
+    //        GetComponent(currentScene);
+   // }
+}
---
+++ LabbyPrototype/Assets/Scripts/Switch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+
+	public Object door;
+	public bool switchActive;
+
+	private void Update()
+	{
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		DestroyObject(door);
+	}
+
+
+
+}
